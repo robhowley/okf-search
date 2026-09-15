2c3fe4d8e3d98291c308e0f24d461d55314c1bb0
Rob Howley <rob.howley@shopify.com>
2026-09-15 07:29:40 -0400

    fix: include open options in native package export validation

--- scripts/check-package.mjs
+++ scripts/check-package.mjs
@@ -65,6 +65,7 @@ const nativeRootTypeExports = [
   "OkfIndexStorageStats",
   "OkfIngestResult",
   "OkfLogicalIndexStats",
+  "OkfOpenOptions",
   "OkfParameter",
   "OkfSearch",
   "OkfSearchField",
